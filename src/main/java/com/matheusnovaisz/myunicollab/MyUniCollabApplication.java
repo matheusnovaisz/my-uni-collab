894b0e0db4bda3a3b98c9beb8d322cc5d3029c34
matheusnovaisz <matheusnovaisz@gmail.com>
2024-09-23 12:16:22 -0300

    start spring project

---
+++ src/main/java/com/matheusnovaisz/myunicollab/MyUniCollabApplication.java
@@ -0,0 +1,13 @@
+package com.matheusnovaisz.myunicollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUniCollabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUniCollabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matheusnovaisz/myunicollab/MyUniCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusnovaisz.myunicollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUniCollabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
